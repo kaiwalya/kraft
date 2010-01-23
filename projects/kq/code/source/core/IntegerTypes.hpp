bbda8616ff6ae780164fb03e6fc9ea7611fc6fcc
Kaiwalya Kher <knkher@gmail.com>
2010-01-23 20:58:07 +0000

    IntegerTypes.hpp, now works with gcc and microsoft compilers.
    git-svn-id: http://subversion.assembla.com/svn/kraft@5 06128d7d-dce7-46cd-bccc-bdfe512662dc

--- projects/kq/code/source/core/IntegerTypes.hpp
+++ projects/kq/code/source/core/IntegerTypes.hpp
@@ -2,21 +2,37 @@
 #define _INTEGER_TYPES_H_
 
 
-#define MakeTypes(i, sz, type) typedef type i##sz; typedef unsigned type u##i##sz
+
+
+#if defined(_MSC_VER)
 
 namespace kq{
-
 namespace core{
-
+#define MakeTypes(i, sz, type) typedef type i##sz; typedef unsigned type u##i##sz
 MakeTypes(i, 8, __int8);
 MakeTypes(i, 16, __int16);
 MakeTypes(i, 32, __int32);
 MakeTypes(i, 64, __int64);
-
 }
-
 }
-
 #undef MakeTypes
+
+#elif defined(__GNUC__)
+
+#include "stdint.h"
+namespace kq{
+namespace core{
+#define MakeTypes(i, sz, type) typedef type i##sz; typedef u##type u##i##sz
+MakeTypes(i, 8, int8_t);
+MakeTypes(i, 16, int16_t);
+MakeTypes(i, 32, int32_t);
+MakeTypes(i, 64, int64_t);
+#undef MakeTypes
+}
+}
+
+#endif
+
+
 
 #endif
