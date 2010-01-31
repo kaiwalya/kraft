5a9d6d7b8b5d91ec72bce01e01a8740e4c97af6a
Kaiwalya Kher <knkher@gmail.com>
2010-01-31 01:22:26 +0000

    Adaptive sleep.
    git-svn-id: http://subversion.assembla.com/svn/kraft@12 06128d7d-dce7-46cd-bccc-bdfe512662dc

--- projects/kq/code/source/main.cpp
+++ projects/kq/code/source/main.cpp
@@ -9,6 +9,226 @@ using namespace kq::core;
 
 #include "windows.h"
 
+
+class RefCounter{
+public:
+    void * object;
+    ui32 count;
+    bool (*AtLast)(RefCounter *);
+
+    RefCounter(void * object = 0, ui32 count = 0, bool (*AtLast)(RefCounter *) = 0){   
+        this->object = object;
+        this->count = count;
+        this->AtLast = AtLast;
+    }
+
+
+    RefCounter(void * object, bool (*AtLast)(RefCounter *)){
+
+        this->count = 0;
+        this->object = object;
+        this->AtLast = AtLast;
+    }
+
+};
+
+static RefCounter nullCounter;
+
+template<typename t>
+class Pointer{
+    RefCounter * m_pRefCounter;
+    void * m_pBufferedObject;
+
+    void setReference(RefCounter * pRefCounter){
+        m_pRefCounter = pRefCounter;
+        m_pBufferedObject = pRefCounter->object;
+    };
+
+    void attach(RefCounter * pRefCounter){
+        setReference(pRefCounter);
+
+        m_pRefCounter->count++;       
+    };
+   
+    void detach(){
+        m_pRefCounter->count--;
+        if(!m_pRefCounter->count && m_pRefCounter->object){
+            if( !(m_pRefCounter->AtLast) || !(*(m_pRefCounter->AtLast))(m_pRefCounter) ){
+                delete (t *)m_pRefCounter->object;
+                delete m_pRefCounter;
+            }
+        }
+        setReference(&nullCounter);
+
+    }
+
+   
+public:
+
+    Pointer(RefCounter * pRefCounter){
+        attach(pRefCounter);
+    };
+
+    Pointer(const Pointer<t> & pointer){
+        attach(pointer.m_pRefCounter);
+    };
+
+    Pointer(){
+        attach(&nullCounter);
+    }
+
+    ~Pointer(){
+        detach();
+    }
+
+    Pointer<t> & operator = (const Pointer<t> & oprand){
+        if(m_pRefCounter->object != oprand.m_pRefCounter->object){
+            detach();
+            attach(oprand.m_pRefCounter);           
+        }
+        return *this;
+    };
+
+    operator Pointer<const t>()const{
+
+        Pointer<const t> ret(m_pRefCounter);
+        return ret;
+    }
+
+    bool operator == (const Pointer<t> & oprand) const{
+        return (oprand.m_pRefCount->object == m_pBufferedObject);
+
+    }
+
+    bool operator != (const Pointer<t> & oprand) const{
+        return (oprand.m_pRefCount->object != m_pBufferedObject);
+    }
+
+    t * operator ->()const {
+        return (t *)(m_pBufferedObject);
+    };
+
+};
+
+class ITicker{
+public:
+    virtual ui64 getTickCount() const = 0;
+    virtual ui64 getTicksPerSecond() const = 0;
+    virtual ~ITicker(){}
+};
+
+class IClock{
+public:
+    virtual i64 waitTillNextCycle() const = 0;
+};
+
+class WindowsAPITicker:public ITicker{
+
+    ui64 iTicksPerSecond;   
+
+public:
+    WindowsAPITicker(){
+
+        LARGE_INTEGER li;
+        if(QueryPerformanceFrequency(&li)){           
+            iTicksPerSecond = li.QuadPart;           
+        }else{
+            iTicksPerSecond = 0;
+           
+        }
+    }
+
+    ui64 getTicksPerSecond() const{
+        return iTicksPerSecond;
+    }
+
+    ui64 getTickCount() const{
+
+        LARGE_INTEGER li;
+        ui64 iTicks;
+        if(QueryPerformanceCounter(&li)){
+            iTicks = li.QuadPart;
+        }else{
+            iTicks = 0;
+        }
+       
+        return iTicks;       
+    }
+};
+
+class IStopWatch:public ITicker{
+public:
+    virtual void reset() = 0;
+};
+
+class StopWatch:public IStopWatch{
+    Pointer<const ITicker> m_pProvider;
+    ui64 m_iInitialTime;
+public:
+    StopWatch(Pointer<const ITicker> pTimeProvider):m_pProvider(pTimeProvider){
+        reset();
+    }
+
+    void reset(){
+        m_iInitialTime = m_pProvider->getTickCount();
+    };
+
+    ui64 getTickCount() const{
+        return m_pProvider->getTickCount() - m_iInitialTime;
+    }
+
+    ui64 getTicksPerSecond() const{
+        return m_pProvider->getTicksPerSecond();
+    };
+};
+
+class ISleeper{
+public:
+    virtual ui64 milliSleep(ui64 nMilli) const = 0;
+};
+
+class WindowsAPISleeper :public ISleeper{
+public:
+    ui64 milliSleep(ui64 nMilli) const{
+        Sleep((DWORD)nMilli);
+        return (DWORD)nMilli;
+    };
+};
+
+class Clock:public IClock{
+
+    Pointer<IStopWatch> m_pStopWatch;
+    Pointer<const ISleeper> m_pSleeper;   
+    double m_dMilliSecondsPerCycle;
+
+public:
+    Clock(double dTargetCyclesPerSecond, Pointer<IStopWatch> pStopWatch, Pointer<const ISleeper> pSleeper){
+        m_dMilliSecondsPerCycle = 1000.0 / dTargetCyclesPerSecond;
+        m_pStopWatch = pStopWatch;
+        m_pSleeper = pSleeper;
+
+    };
+
+    i64 waitTillNextCycle() const{
+        double dTicksPerSecond = (double)m_pStopWatch->getTicksPerSecond();
+        double dConsumedTicksThisCycle = (double)m_pStopWatch->getTickCount();
+        
+
+        double dConsumedMilliSecondsThisCycle = 1000.0 * dConsumedTicksThisCycle / dTicksPerSecond;
+        double dRemainingMilliSecondsThisCycle = m_dMilliSecondsPerCycle - dConsumedMilliSecondsThisCycle;
+
+        if(dRemainingMilliSecondsThisCycle >= 1.0){
+            m_pSleeper->milliSleep((ui64)dRemainingMilliSecondsThisCycle);           
+        }
+		m_pStopWatch->reset();
+        return (i64)dRemainingMilliSecondsThisCycle;
+
+    }
+
+};
+
+
+
 class WindowsConsoleOutputStream{
 
 	HANDLE m_hOut;
@@ -116,6 +336,8 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
 	WindowsConsoleOutputStream console;
 	WindowsConsoleOutputStream * pConsole = &console;
 	pConsole->output("Console Init...\tAttached.\n");
+
+	Pointer<IClock> pClock(new RefCounter(new Clock(30, new RefCounter(new StopWatch(new RefCounter(new WindowsAPITicker()))), new RefCounter(new WindowsAPISleeper))));
 
 
 	DISPLAY_DEVICE dd = {0};
@@ -243,16 +465,12 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
 												break;
 										};
 
-										//Process All Messages in queue
+										//Go back and process all messages in queue before proceeding
 										continue;
 									}
 
 									SwapBuffers(hMainWindowDC);
-
-																		
-									{
-										Sleep(100);
-									}
+									pClock->waitTillNextCycle();								
 								}
 
 								wglMakeCurrent(hMainWindowDC, hGLOld);
