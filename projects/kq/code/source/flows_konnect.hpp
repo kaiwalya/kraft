cde94a7228629782df34a249a69e5592ebadc60d
Kaiwalya Kher <knkher@gmail.com>
2010-08-05 01:03:09 +0000

    Added a Graph interface
    git-svn-id: http://subversion.assembla.com/svn/kraft@28 06128d7d-dce7-46cd-bccc-bdfe512662dc

--- projects/kq/code/source/flows_konnect.hpp
+++ projects/kq/code/source/flows_konnect.hpp
@@ -4,7 +4,50 @@
 #include "core_IntegerTypes.hpp"
 #include "core_memory_RefCounter.hpp"
 
-class IBlock{
+namespace kq{
+	namespace konnect{
+		
+		template<typename NodeID>
+		class IGraph{			
+		
+			
+			class IModifier{
+				enum Error{
+					errNone = 0,
+					errUnknown,
+					errBadAPIUsage,
+				};
+
+				enum Mode{
+					modeChain,
+					modePair,
+					modeOneToMany,
+					modeManyToOne,
+					modeClique,
+				};
+
+				switchMode(Mode m);
+				virtual Error node(NodeID id) = 0;
+				virtual Error nodeVector(NodeID * pNodes, kq::core::ui32 nNodes) = 0;
+				virtual Error nodeString(NodeID * pNodes) = 0;
+			};
+
+			enum ModifierType{
+				modConnect,
+				modDisconnect,
+			};
+
+			class IViewer{								
+			};
+
+			
+			virtual kq::core::memory::Pointer<IMutator> getMutator(ModifierType type) = 0			
+			virtual kq::core::memory::Pointer<IReader> getViewer() = 0
+			
+
+		};
+
+	};
 };
 
 #endif
